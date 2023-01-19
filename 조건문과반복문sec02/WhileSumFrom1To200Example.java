af4b17e42f89331162be1a08c4806697fa54b9c5
DESKTOP-O5UAK6C\user <k.do.dodong@gmail.com>
2023-01-19 15:40:14 +0900

    조건문과반복문

---
+++ 조건문과반복문sec02/WhileSumFrom1To200Example.java
@@ -0,0 +1,19 @@
+package 조건문과반복문sec02;
+
+public class WhileSumFrom1To200Example {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		int i = 1;
+		
+		while(i<=100) {
+			sum += i;
+			i++;
+		}
+		
+		System.out.println("1~" + (i-1) + " 합 : " + sum);
+
+	}
+
+}
