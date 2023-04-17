420b53bd748a2fe971689c31728265cb3830551e
DESKTOP-O5UAK6C\user <k.do.dodong@gmail.com>
2023-04-17 14:58:03 +0900

    상수 및 형 변환

---
+++ Intellij Java/JavaWorkspace/src/chap_01/_07_TypeCasting.java
@@ -0,0 +1,12 @@
+package chap_01;
+
+public class _07_TypeCasting {
+    public static void main(String[] args) {
+        // 형변환 TypeCasting
+        // 정수형에서 실수형으로
+        // 실수형에서 정수형으로
+
+        int score = 93;
+        System.out.println(score); // 93
+    }
+}
