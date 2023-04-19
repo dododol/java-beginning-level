cc6fb92154e33d4e9f359e936c6d20a6b8367fb6
DESKTOP-O5UAK6C\user <k.do.dodong@gmail.com>
2023-04-19 11:16:55 +0900

    형변환

--- Intellij Java/JavaWorkspace/src/chap_01/_07_TypeCasting.java
+++ Intellij Java/JavaWorkspace/src/chap_01/_07_TypeCasting.java
@@ -6,7 +6,15 @@ public class _07_TypeCasting {
         // 정수형에서 실수형으로
         // 실수형에서 정수형으로
 
+        // int to float, double
         int score = 93;
         System.out.println(score); // 93
+        System.out.println((float) score); //93.0
+        System.out.println((double) score); //93.0
+
+        // float, double to int
+        float score_f = 93.3F;
+        double score_d = 98.8;
+        System.out.println((int) score_f); // 93
     }
 }
