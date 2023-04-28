5218b647bd1b5374f799795987457417fc672660
DESKTOP-O5UAK6C\user <k.do.dodong@gmail.com>
2023-04-28 10:41:26 +0900

    .

--- Intellij Java/JavaWorkspace/src/chap_02/_05_Operator5.java
+++ Intellij Java/JavaWorkspace/src/chap_02/_05_Operator5.java
@@ -3,7 +3,7 @@ package chap_02;
 public class _05_Operator5 {
     public static void main(String[] args){
         // 삼항 연산자
-        //결과 = (조건) ? (참의 경우 결과값) : (거짓의 경우 결과값)
+        // 결과 = (조건) ? (참의 경우 결과값) : (거짓의 경우 결과값)
 
         int x = 3;
         int y = 5;
