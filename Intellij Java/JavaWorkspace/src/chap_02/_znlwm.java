1dea8c6127283c82a4da86a6274ee43eab461553
DESKTOP-O5UAK6C\user <k.do.dodong@gmail.com>
2023-04-29 22:37:07 +0900

    연산자 퀴즈

---
+++ Intellij Java/JavaWorkspace/src/chap_02/_znlwm.java
@@ -0,0 +1,11 @@
+package chap_02;
+
+public class _znlwm{
+    public static void main(String[] args) {
+
+        int x = 121;
+        String y = (x >= 120) ? "탑승 가능합니다." : "탑승 불가능합니다.";
+                System.out.println("키가 "+ x + "cm 이므로 " + y);
+
+    }
+}
