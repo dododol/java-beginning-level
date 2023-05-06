8e7976cd187f939326b86200a2df68b7ba767a72
dododonng <k.do.dodong@gmail.com>
2023-05-06 11:16:32 +0900

    문자열. indexOf를 통하여 출력하고 싶은 부분만 출력하기

--- Intellij Java/JavaWorkspace/src/chap_03/_02_String2.java
+++ Intellij Java/JavaWorkspace/src/chap_03/_02_String2.java
@@ -9,7 +9,7 @@ public class _02_String2 {
         System.out.println(s.substring(7)); // 인덱스 기준 7 부터 시작 (이전 내용은 삭제)
         System.out.println(s.substring(s.indexOf("Java")));
         // "Java" 가 시작하는 위치부터, "." 이 시작하는 위치 바로 앞까지
-        System.out.println(s.substring(s.indexOf("Java"), s.indexOf("."))); // 시작 위치부터 끝 위치 "직적"
+        System.out.println(s.substring(s.indexOf("Java"), s.indexOf("."))); // 시작 위치부터 끝 위치 "직전" 까지
 
         // 공백 제거
         s = "       I love Java.      ";
